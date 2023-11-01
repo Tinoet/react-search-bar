9ff763cddc9491417e9ecdb7bee4a13f564ccb0f
Elijah Tochkwu <elijahtinoet@gmail.com>
2023-11-01 16:09:25 +0100

    Add files via upload

---
+++ src/Components/SearchResult.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import "./SearchResult.css";
+
+export const SearchResult = ({ result }) => {
+  return (
+    <div
+      className="search-result"
+      onClick={(e) => alert(`You selected ${result}!`)}
+    >
+      {result}
+    </div>
+  );
+};
